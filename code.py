feb2c45f95f3b997ccfefa20d3ed8aa9efd25d24
sohambasu1995 <70295332+sohambasu1995@users.noreply.github.com>
2020-09-06 17:24:20 +0530

    Update code.py
    Entered the digit check

--- code.py
+++ code.py
@@ -9,23 +9,27 @@ a=string.ascii_letters
 p=permutations(s)
 lst=list()
 
-#Create a dictionary
-d=[]
-lst=list(p)
-el=lst[0]
+try:
+	d=int(s)
+	print("Please enter only string/word")
+except:
+	#Create a dictionary
+	d=[]
+	lst=list(p)
+	el=lst[0]
 
-#Create a flag
-chk=True
+	#Create a flag
+	chk=True
 
-#Check if no permutation of the list is possible, i.e., the string/word contains of a single alphabet
-for item in lst:
-	if el!=item:
-		chk=False
-		break
+	#Check if no permutation of the list is possible, i.e., the string/word contains of a single alphabet
+	for item in lst:
+		if el!=item:
+			chk=False
+			break
 
-#Print the desired output
-if(chk==True):print("No permutation possible")
-else:
-	print(lst[1])
+	#Print the desired output
+	if(chk==True):print("No permutation possible")
+	else:
+		print(lst[1])
 
 #Code ©SohamBasu
